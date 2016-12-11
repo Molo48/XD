26f840ab1a1227e34326f5d4e92638c4cd3eea93
Molo <mat.ivki@gmail.com>
2016-12-11 17:26:18 +0100

    dz

--- skripta.js
+++ skripta.js
@@ -2,12 +2,8 @@ var appModule =angular.module("app1", []);
 
 appModule.controller("imeControllera", function($scope){
     
-    $scope.varijabla ="neki tekst za prikazati u viewu";
-    $scope.broj1='';
-    $scope.broj2='';
+    $scope.broj1=null;
+    $scope.broj2=null;
     
-    $scope.zbroj=function() {
-        return $scope.broj1+$scope.broj2;
-    };
         
 });
