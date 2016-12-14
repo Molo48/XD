def9f590ef1ff7bf8124bb83ddd7cea113c421b2
Molo48 <skulldead.hex@gmail.com>
2016-12-14 14:33:22 +0100

    nedovrseni dz

---
+++ DZ/helpController.js
@@ -0,0 +1,19 @@
+appModule.controller("helpController", function ($scope) {
+
+	$scope.profil = {
+        firstname: "",
+        lastname: "",
+        age: "",
+        inventar: []
+    };
+   
+      $scope.text = '';
+      $scope.submit = function() {
+        if ($scope.text) {
+          $scope.profil.inventar.push(this.text);
+          $scope.text = '';
+        }
+      };
+    
+});
+
---
+++ DZ/skripta.js
@@ -0,0 +1,16 @@
+// dokumentacija na https://docs.angularjs.org/api
+
+var appModule = angular.module("app1", ["ngRoute"]);
+
+appModule.config(function ($routeProvider, $locationProvider) {
+
+	$routeProvider.when("/home", {
+		templateUrl: "home.html",
+		controller: "homeController"
+	}).when("/help", {
+		templateUrl: "help.html",
+		controller: "helpController"
+	}).otherwise({
+        redirectTo: '/home'
+    });
+})
