64d8ae1c4969c3b0e4aebb9818de49394802f6c1
Molo <mat.ivki@gmail.com>
2016-12-17 23:43:28 +0100

    basic function done

---
+++ DZ/directives/dateDisplay/dateDisplay.js
@@ -0,0 +1,16 @@
+appModule.directive("dateDisplay",function(){
+
+	function dateDisplayController($scope){
+		$scope.dateNow=new Date();
+
+	}
+
+
+	return {
+		controller: dateDisplayController,
+		templateUrl: "directives/dateDisplay/dateDisplay.html",
+		scope: {
+			additionalText:"@"
+		}
+	};
+});
--- DZ/pages/profil/profilController.js
+++ DZ/pages/profil/profilController.js
@@ -4,15 +4,57 @@ appModule.controller("profilController", function ($scope) {
         firstname: "",
         lastname: "",
         age: "",
-        inventar: []
-    };
+       inventar: []
+        };
+
+    //     inventar:[{
+    //       naziv:"",
+    //       serBr:"",
+    //       knVal:"",
+    //       opis:"",
+    //       dostup:""
+    //       }]
+        
+    // };
    
+
+
+      $scope.tempNaz = '';
+      $scope.tempSer = '';
+      $scope.tempVal = '';
+      $scope.tempOpis = '';
+      $scope.tempDost = '';
+
+      // $scope.submit = function(){
+      // if($scope.tempNaz && $scope.tempSer && $scope.tempVal && $scope.tempOpis && $scope.tempDost){
+      //       $scope.profil.ivnentar.naziv=$scope.tempNaz;
+      //       $scope.profil.ivnentar.serBr=$scope.tempSer;
+      //       $scope.profil.ivnentar.SerBr=$scope.tempVal;
+      //       $scope.profil.ivnentar.knVal=$scope.tempOpis;
+      //       $scope.profil.ivnentar.opis=$scope.tempDost;
+      //       $scope.profil.ivnentar.dostup=$scope.tempDost;
+      //       $scope.tempNaz = '';
+      //       $scope.tempSer = '';
+      //       $scope.tempVal = '';
+      //       $scope.tempOpis = '';
+      //       $scope.tempDost = '';
+      //       }
+      // };
+
+
       $scope.text = '';
+      
       $scope.submit = function() {
-        if ($scope.text) {
-          $scope.profil.inventar.push(this.text);
-          $scope.text = '';
-        }
+        if($scope.tempNaz && $scope.tempSer && $scope.tempVal && $scope.tempOpis){
+          $scope.profil.inventar.push({
+          naziv : $scope.tempNaz,
+          serBr : $scope.tempSer,
+          knVal : $scope.tempVal,
+          opis : $scope.tempOpis,
+          dostup : $scope.tempDost
+          });
+          }
+        
       };
     
 });
