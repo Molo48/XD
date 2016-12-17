8c82202a8b621f8bfade6c821732a19134c9460f
Molo <mat.ivki@gmail.com>
2016-12-17 17:37:21 +0100

    refactoring

--- DZ/skripta.js
+++ DZ/skripta.js
@@ -3,10 +3,10 @@ var appModule = angular.module("app1", ["ngRoute"]);
 appModule.config(function ($routeProvider, $locationProvider) {
 
 	$routeProvider.when("/home", {
-		templateUrl: "home.html",
+		templateUrl: "pages/home/home.html",
 		controller: "homeController"
 	}).when("/profil", {
-		templateUrl: "profil.html",
+		templateUrl: "pages/profil/profil.html",
 		controller: "profilController"
 	}).otherwise({
         redirectTo: '/home'
