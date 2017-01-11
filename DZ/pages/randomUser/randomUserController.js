ed6cebc20f5b7fe04b9f34174ee55d5d3cd338db
Molo48 <skulldead.hex@gmail.com>
2017-01-11 14:55:45 +0100

    New page

---
+++ DZ/pages/randomUser/randomUserController.js
@@ -0,0 +1,8 @@
+//moramo je uključit u index.html
+appModule.controller("randomUserController", function ($scope) {
+	// kod koji se bude nalazio unutar tijela ove funkcije je kod 'homeController' kontrolera
+
+	$scope.broj1=null;
+    $scope.broj2=null;
+    
+});
--- DZ/skripta.js
+++ DZ/skripta.js
@@ -8,6 +8,9 @@ appModule.config(function ($routeProvider, $locationProvider) {
 	}).when("/profil", {
 		templateUrl: "pages/profil/profil.html",
 		controller: "profilController"
+    }).when("/randomUser", {
+		templateUrl: "pages/randomUser/randomUser.html",
+		controller: "randomUserController"
 	}).otherwise({
         redirectTo: '/home'
     });
